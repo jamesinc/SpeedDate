1b308423501ca142c314bc081e334e8c7a40cae3
James Ducker <james.ducker@gmail.com>
2012-07-10 12:56:20 +1000

    Added support for nautical designators in ISO8601 strings passed into the fromISO8601 function. Fixed a bug in the same function related to timezone offsets.

--- speeddate-1.0.js
+++ speeddate-1.0.js
@@ -25,7 +25,7 @@
  *
  * Date: 2010-06-12T15:13:23+1000
  *
- * Last modified: 2012-07-10T11:55:23+1000
+ * Last modified: 2012-07-10T12:55:33+1000
  */
 
 (function() {
@@ -127,56 +127,85 @@
         // 2010-168 [Year, day of year]
         //
         // To do: improve this regex (maybe), to better enforce ISO8601 formatting.
-        var regexp = /([0-9]{4})(-([W0-9]{1,3})(-([0-9]{1,2})([T|\s]([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?(Z|(([-+])([0-9]{2})((:?)([0-9]{2}))?))?)?)?)?/,
+        var regexp = /([0-9]{4})(-([W0-9]{1,3})(-([0-9]{1,2})([T|\s]([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?([A-IK-Z]|(([-+])([0-9]{2})((:?)([0-9]{2}))?))?)?)?)?/,
             dateParts = isoDate.match(new RegExp(regexp)),
             date = new Date(dateParts[1], 0, 1),
-            offset = 0,
+            // system-local timezone
+            offset = date.getTimezoneOffset(),
             time;
 
-        if (dateParts[3] && dateParts[3].indexOf("W") === -1)
-            if (dateParts[5])
+
+        // timezones are expressed as offsets from UTC.
+        if ( dateParts[13] ) {
+            if ( dateParts[13].length === 1 ) {
+                console.log("picked up a nautical designator");
+                offset += ( Date.timezoneFromNauticalDesignator(dateParts[13]) * 60 );
+                console.log( Date.timezoneFromNauticalDesignator(dateParts[13]) * 60 );
+            }
+        }
+
+        if ( dateParts[3] && dateParts[3].indexOf("W") === -1 )
+            if ( dateParts[5] )
                 date.setMonth(dateParts[3] - 1);
             else
                 return new Date((dateParts[3] - 1) * MS_IN_DAY + date.getTime());
 
-        if (dateParts[5]) {
+        if ( dateParts[5] ) {
             if ( dateParts[3].indexOf("W") === 0 && dateParts[5]) {
-                return new Date(date.getTime() + (dateParts[3].substr(1, dateParts[3].length - 1) - 1) * 7 * MS_IN_DAY + (dateParts[5] - 1) * MS_IN_DAY);
+                return new Date( date.getTime() + (dateParts[3].substr(1, dateParts[3].length - 1) - 1) * 7 * MS_IN_DAY + (dateParts[5] - 1) * MS_IN_DAY );
             } else
                 date.setDate(dateParts[5]);
         }
 
-        if (dateParts[7]) date.setHours(dateParts[7]);
-        if (dateParts[8]) date.setMinutes(dateParts[8]);
-        if (dateParts[10]) date.setSeconds(dateParts[10]);
-        if (dateParts[12]) date.setMilliseconds(Number("0." + dateParts[12]) * 1000);
-        if (dateParts[14]) offset = ((Number(dateParts[16]) * 60) + ((typeof dateParts[19] !== "undefined") ? Number(dateParts[19]) : 0)) * ((dateParts[15] === "-") ? 1 : -1);
+        if ( dateParts[7] ) date.setHours(dateParts[7]);
+        if ( dateParts[8] ) date.setMinutes(dateParts[8]);
+        if ( dateParts[10] ) date.setSeconds(dateParts[10]);
+        if ( dateParts[12] ) date.setMilliseconds(Number("0." + dateParts[12]) * 1000);
+        if ( dateParts[14] ) offset += ( Number(dateParts[14]) / 100 * 60 );
 
-        offset -= date.getTimezoneOffset();
-        time = (Number(date) + (offset * 60 * 1000));
+        time = ( date.getTime() - (offset * 60 * 1000) );
 
-        date.setTime(Number(time));
+        date.setTime(time);
 
         return date;
     };
 
     // Use if you need to get timezone based on a nautical identifier
     // i.e. K (Kilo time) is the identifier for +1000 (i.e. AEST)
-    Date.timezoneFromNauticalDesignator = function ( phonetic ) {
-        var code = phonetic.toUpperCase().charCodeAt(0);
+    // Provides results in +/- hours as integer types.
+    // e.g.
+    //      Date.timezoneFromNauticalDesignator("K") =>  10
+    //      Date.timezoneFromNauticalDesignator("Z") =>   0
+    //      Date.timezoneFromNauticalDesignator("P") => - 3
 
-        // Zulu (also catches Juliet time, which is unassigned)
-        if ( code === 90 || code === 74 )
-            return 0;
-        // Alpha (+1) to India (+9)
-        if ( code >= 65 && code <= 73 )
-            return (code - 64);
-        // Kilo (+10) to Mike (+12)
-        if ( code >= 75 && code <= 77 )
-            return (code - 65);
-        // November (-1) to Yankee (-12)
-        if ( code >= 78 && code <= 89 )
-            return (0 - (code-77));    
+    Date.timezoneFromNauticalDesignator = function ( phonetic ) {
+
+        var code,
+            timezone = null;
+
+        if ( phonetic.length ) {
+
+            code = phonetic.toUpperCase().charCodeAt(0);
+
+            // Zulu (also catches Juliet time, which is unassigned)
+            if ( code === 90 || code === 74 )
+                timezone = 0;
+
+            // Alpha (+1) to India (+9)
+            if ( code >= 65 && code <= 73 )
+                timezone = (code - 64);
+
+            // Kilo (+10) to Mike (+12)
+            if ( code >= 75 && code <= 77 )
+                timezone = (code - 65);
+
+            // November (-1) to Yankee (-12)
+            if ( code >= 78 && code <= 89 )
+                timezone = (0 - (code-77));    
+
+        }
+
+        return timezone;
     };
 
 })();
